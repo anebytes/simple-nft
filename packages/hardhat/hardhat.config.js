3abfdc430e0a059a6df418b45e8631647a4ae0e0
Yande <yandmchl@gmail.com>
2023-01-15 14:56:31 +0000

    init

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -20,7 +20,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 */
 
 // Select the network you want to deploy to here:
-const defaultNetwork = "localhost";
+const defaultNetwork = "goerli";
 
 const mainnetGwei = 115;
 
@@ -85,9 +85,9 @@ module.exports = {
     goerli: {
       url: "https://goerli.infura.io/v3/ec6a8acd1d354717acec099ad46a0bab", // <---- YOUR INFURA ID! (or it won't work)
       //      url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXXXX/eth/goerli", // <---- YOUR MORALIS ID! (not limited to infura)
-      accounts: {
+      accounts: [{
         mnemonic: mnemonic(),
-      },
+      }],
     },
     xdai: {
       url: "https://rpc.xdaichain.com/",
